a3a7e3dfb5bc07ba2c5ce8fe7a137d484c5b3b48
bravo1goingdark <kumarashutosh34169@gmail.com>
2023-12-18 15:20:46 +0530

    triangular sum of an array

--- Arrays/triangularsum.java
+++ Arrays/triangularsum.java
@@ -1,5 +1,18 @@
 public class triangularsum {
-    public int triangularSum(int[] nums) {
-        
+    public static void main(String[] args) {
+        System.out.println(triangularSum(new int[]{1,2,3,4,5}));
+    }
+    public static int triangularSum(int[] nums) {
+        while (nums.length > 1) {
+            int[] newarr = new int[nums.length - 1];
+
+            for (int i = 0; i < nums.length - 1; i++) {
+                newarr[i] = (nums[i] + nums[i + 1]) % 10;
+            }
+
+            nums = newarr; // Update the nums array for the next iteration
+        }
+
+        return nums[0];
     }
 }
